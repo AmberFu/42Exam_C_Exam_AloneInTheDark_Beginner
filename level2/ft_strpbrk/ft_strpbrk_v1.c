7c5322ee217df9919b9252708bc250c0c19f6929
Po-ying Fu <pfu@e1z2r7p13.42.us.org>
2019-11-19 16:03:52 -0800

    add trace and new version of ft_strpbrk.c

--- level2/ft_strpbrk/ft_strpbrk_v1.c
+++ level2/ft_strpbrk/ft_strpbrk_v1.c
@@ -5,39 +5,39 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/19 09:15:16 by exam              #+#    #+#             */
-/*   Updated: 2019/11/19 09:55:35 by exam             ###   ########.fr       */
+/*   Created: 2019/08/20 09:47:36 by exam              #+#    #+#             */
+/*   Updated: 2019/08/20 10:13:38 by exam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-int		ft_strlen(char *s)
+int		ft_haschar(const char *s, char c)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
-	while (*s++ != '\0')
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (1);
 		i++;
-	return (i);
+	}
+	return (0);
 }
 
-char    *ft_strpbrk(const char *s1, const char *s2)
+char	*ft_strpbrk(const char *s1, const char *s2)
 {
-	int		len_s2;
-	int		i;
+	char	*str;
+	unsigned int	i;
 
-	len_s2 = ft_strlen((char *)s2);
-	while (*s1 != '\0')
+	i = 0;
+	if (!s1 || !s2)
+		return (0);
+	str = (char *)s1;
+	while (*str)
 	{
-		i = 0;
-		while (i < len_s2)
-		{
-			if (*s1 == s2[i])
-				return ((char *)s1);
-			i++;
-		}
-		s1++;
+		if (ft_haschar(s2, *str))
+			return (str);
+		str++;
 	}
-	return (NULL);
+	return (0);
 }
